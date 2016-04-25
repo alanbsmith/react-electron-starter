a25762a9fe862bc0375cc173905d895ed5843649
Alan Smith <a.bax.smith@gmail.com>
2016-04-25 16:34:09 -0600

    initial commit | basic setup for React + Electron

---
+++ README.md
@@ -0,0 +1,14 @@
+# React + Electron Starter
+_an example app for using React with Electron_
+
+### UP & RUNNING
+* Run `npm install`
+
+**FOR DEVELOPMENT**
+* Run `npm run dev` to fire up Webpack
+* In another terminal window run `npm start` to start electron
+
+**FOR PRODUCTION**
+* Run `npm run postinstall` to compile all your assets into `dist/bundle.js`
+* Change the script tag in `dist/index.html` to use `bundle.js` as its source
+* Run `npm start` to start electron
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  module: {
+    loaders: [
+      { test: /\.js?$/, loader: 'babel', exclude: /node_modules/ },
+      { test: /\.s?css$/, loader: 'style!css!sass' },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+};
